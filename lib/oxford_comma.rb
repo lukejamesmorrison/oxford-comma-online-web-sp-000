d2c441cff63e925778ae28b371962fd8bf58eb87
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 17:43:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
   array.collect do |item|
-
+    if array.first == item
+      item
+    elsif array.last = item
+      "and ${item}"
+    else
+      ", ${item}"
+    end
   end
 end
