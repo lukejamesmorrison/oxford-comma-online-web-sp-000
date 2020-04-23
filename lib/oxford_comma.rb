5780c65c4ebd04c7b2aaf204ecfc2fcaabb85a93
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 17:42:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,5 @@
 def oxford_comma(array)
-  if array.length  <= 1
-    array.join
-  else
-    array.collect_with_index do |item, index|
-      puts index
-      if index == 0
-        item
-      elsif index != array.length - 1
-        item = item + ', '
-      else
-        item = " and " + item
-      end
-    end
+  array.collect do |item|
 
-    array.join
   end
 end
